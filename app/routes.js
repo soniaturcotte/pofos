e1dfd599c8855663d672872aa100af0dbcd48ffa
sonia.turcotte@gmail.com <sonia.turcotte@gmail.com>
2017-04-25 15:18:06 +0100

    Combine admin flow for 1 and 2
    *Routes for custom payment amount
    *Option to add multiple items
    *Show page content and custom paragraph
    *Follow Pay confirmation box pattern for success page

--- app/routes.js
+++ app/routes.js
@@ -9,3 +9,19 @@ router.get('/', function (req, res) {
 // add your routes here
 
 module.exports = router
+
+
+// Admin routing v1 & 2 
+
+router.post('/admin/1/settings', function (req, res) {
+  // get the answer from the query string 
+  var fixedFee = req.body.fixedFee
+	console.log(fixedFee);
+  if (fixedFee === 'false') {
+    // redirect to the relevant page
+    res.redirect('content')
+  } else {
+    // if fixedFee is any other value (or is missing) render the page requested
+    res.redirect('items')
+  }
+})
